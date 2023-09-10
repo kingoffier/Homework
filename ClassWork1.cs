d7c03958f796a4da52caad50ae44e912ff726939
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-09-11 00:24:00 +0500

    Add files via upload

---
+++ ClassWork1.cs
@@ -0,0 +1,25 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.4.33213.308
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "ChyProgect", "ChyProgect\ChyProgect.csproj", "{AEA403A6-701D-46E0-A405-A935DF87C115}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{AEA403A6-701D-46E0-A405-A935DF87C115}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{AEA403A6-701D-46E0-A405-A935DF87C115}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{AEA403A6-701D-46E0-A405-A935DF87C115}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{AEA403A6-701D-46E0-A405-A935DF87C115}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {3D2DEB41-1218-457F-984E-76775494E5D1}
+	EndGlobalSection
+EndGlobal
